b6c47762ca40a949da3d0f054f7d5a6995e87e7a
Diane Huang <qianhuang11@gmail.com>
2018-06-16 02:38:15 -0400

    New toggle

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.js-health-container-toggle').addEventListener('click', () => {
+        document.querySelector('.js-health-container').classList.remove('hidden')
+        document.querySelector('.js-item-container').classList.add('hidden')
+    })
+
+    document.querySelector('.js-item-container-toggle').addEventListener('click', () => {
+        document.querySelector('.js-item-container').classList.remove('hidden')
+        document.querySelector('.js-health-container').classList.add('hidden')
+    })
+});
+
